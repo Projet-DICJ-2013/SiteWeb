2e11c25e5dba2b3ef2f8b6f3944402d805988b1d
David <brulopropane@hotmail.com>
2013-11-22 15:20:59 -0500

    ...

--- Demande_Pret.aspx.vb
+++ Demande_Pret.aspx.vb
@@ -5,20 +5,22 @@ Imports System.Linq
 Partial Class Demande_Pret
     Inherits System.Web.UI.Page
 
-    Private req As IQueryable(Of ExemplaireModele)
+    Private req As IQueryable(Of tblExemplaire)
 
-    Public Class ExemplaireModele
-        Property Exemplaire As tblExemplaire
-        Property Modele As tblModele
-    End Class
 
     Sub Page_Load() Handles MyBase.Load
         Dim BD As New PresenceModelEntities
         req = (From r In BD.tblExemplaire
-             Join m In BD.tblModele
-             On m.NoModele Equals r.NoModele
-             Where r.TypeEtat <> "Supprimé"
-       Select New ExemplaireModele With {.Exemplaire = r, .Modele = m})
-        drpModele.DataSource = req
+        Where r.TypeEtat <> "Supprimé"
+       Select r)
+
+
+        drpModele.DataSource = req.ToList
+        drpModele.DataValueField = "NoModele"
+        drpModele.DataBind()
+
+        drpModele.Items.Insert(0, New ListItem(String.Empty, String.Empty))
+        drpModele.SelectedIndex = 0
+
     End Sub
 End Class
