c027add4dc645374345c10ad3f9cbe46e29498f1
David <brulopropane@hotmail.com>
2013-11-22 14:35:43 -0500

    Ajout de la page demande pret

---
+++ Demande_Pret.aspx.vb
@@ -0,0 +1,8 @@
+﻿
+Partial Class Demande_Pret
+    Inherits System.Web.UI.Page
+
+    Sub Page_Load() Handles MyBase.Load
+
+    End Sub
+End Class
--- Prets.aspx.vb
+++ Prets.aspx.vb
@@ -2,4 +2,8 @@
 Partial Class Prets
     Inherits System.Web.UI.Page
 
+    Sub btnDemande_Click()
+        Response.Redirect("Demande_Pret.aspx")
+    End Sub
+
 End Class
