4a68d141b766a8364999a4320b9bac7dde4350ae
David <brulopropane@hotmail.com>
2013-11-27 10:24:10 -0500

    Demandepret

--- Demande_Pret.aspx.vb
+++ Demande_Pret.aspx.vb
@@ -20,30 +20,43 @@ Partial Class Demande_Pret
         Dim BD As New PresenceModel
         req = (From r In BD.tblExemplaire
                    Join m In BD.tblModele On m.NoModele Equals r.NoModele
-            Where r.TypeEtat <> "Supprimé"
-            Select m) 'New ExemplaireModele With {.Exemplaire = r, .Modele = m})
+        Where r.TypeEtat <> "Supprimé"
+            Select m)
 
 
-        drpModele.DataSource = req.ToList()
+        'drpModele.DataSource = req.ToList()
+        'drpModele.DataValueField = "NoModele"
 
-
-        drpModele.DataValueField = "NoModele"
-        drpModele.DataBind()
-
-        drpModele.Items.Insert(0, New ListItem(String.Empty, String.Empty))
-        drpModele.SelectedIndex = 0
 
     End Sub
 
     Sub OnType_Modified() Handles DropDownList1.SelectedIndexChanged
 
         req = req.Where(Function(r) CType(r.TypeMachine, String).Contains(DropDownList1.SelectedItem.Text))
-        If req.ToList.Count > 0 Then
-            drpModele.DataSource = req.ToList
-            drpModele.DataBind()
-        Else
-            drpModele.Items.Clear()
-        End If
+        'TablePret.Rows.Clear()
+        For Each modele As tblModele In req
+            Dim NouvelleLigne As New TableRow
+            Dim NouvelleCell As New TableCell
+            Dim marquecell As TableCell = New TableCell With {.Text = modele.Marque}
+            NouvelleLigne.Cells.Add(marquecell)
+            Dim modelecell As TableCell = New TableCell With {.Text = modele.NoModele}
+            NouvelleLigne.Cells.Add(modelecell)
+            Dim notecell As TableCell = New TableCell With {.Text = modele.NoteModele}
+            NouvelleLigne.Cells.Add(notecell)
+            Dim garantiecell As TableCell = New TableCell With {.Text = modele.NbAnneeGarantie}
+            NouvelleLigne.Cells.Add(garantiecell)
+            TablePret.Rows.Add(NouvelleLigne)
+        Next
+
+
+        'If req.ToList.Count > 0 Then
+        '    drpModele.DataSource = req.ToList
+        '    drpModele.DataBind()
+        '    drpModele.Items.Insert(0, New ListItem("Choisir un modèle", String.Empty))
+        '    drpModele.SelectedIndex = 0
+        'Else
+        '    drpModele.Items.Clear()
+        'End If
 
     End Sub
 
