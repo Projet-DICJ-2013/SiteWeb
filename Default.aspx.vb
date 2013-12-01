bce8ee659e84a24cb302d6f12d016dc4214e3174
PPears <patlambo90@hotmail.com>
2013-12-01 11:26:50 -0500

    Test

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -1,33 +1,62 @@
 ﻿Imports System.Web.DynamicData
+Imports System.Windows.Data
 
 Class _Default
     Inherits Page
 
+    Private News As New MesNews
+    Private i As Integer = 0
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
 
+        AfficheActu()
 
+    End Sub
 
+    Public Sub btnPrec_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+        News.lstActu.MoveCurrentToPrevious()
+        AfficheActu()
 
 
     End Sub
-    
+
+    Public Sub btnNext_Click(ByVal sender As Object, ByVal e As EventArgs)
+
+        News.lstActu.MoveCurrentToNext()
+        AfficheActu()
+
+    End Sub
+
+    Private Sub AfficheActu()
+        lblNew.Text = CType(News.lstActu.CurrentItem, tblActualite).TexteActu
+        Context.Session.
+    End Sub
 End Class
 
 
-Class LstNews
+Class MesNews
 
-    Private LstNews
+    Private MesNews As ListCollectionView
+    Private BD As New PresenceModel
 
-    Public Property News
+    Public Property lstActu As ListCollectionView
 
         Get
-
+            Return MesNews
         End Get
-        Set(value)
-
+        Set(value As ListCollectionView)
+            MesNews = value
         End Set
     End Property
 
+    Public Sub New()
 
+        Dim lstNews = (From News In BD.tblActualite
+                    Order By News.IDActualite Descending
+                    Select News).ToList
+
+
+        lstActu = New ListCollectionView(lstNews)
+    End Sub
 End Class
